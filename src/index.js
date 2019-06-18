9c0dc424cdfac011e0e8aa51d2c1298b020728e4
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-06-18 16:33:16 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AppLang from "./lang-context/AppLang";
+
+ReactDOM.render(
+<AppLang />, 
+document.getElementById("root"));
